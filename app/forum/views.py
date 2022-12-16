b8b51dfe4d45472a35e0f51ae7d272eb19b54f3a
Josh Bright <josh.bright@gmail.com>
2022-12-16 14:54:35 -0800

    We now render the list of forum objects on the index page

--- app/forum/views.py
+++ app/forum/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def index(request):
-    return render(request, "forum/index.html", {})
+    forum_list = models.Forum.objects.all()
+    return render(request, "forum/index.html", {"forum_list": forum_list})
+
