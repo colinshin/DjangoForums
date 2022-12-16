99866e223e3c59a530c13d6651f214b0e63943c8
Josh Bright <josh.bright@gmail.com>
2022-12-16 15:48:33 -0800

    Added forum detail view and url

--- app/forum/urls.py
+++ app/forum/urls.py
@@ -3,4 +3,5 @@ from forum import views
 
 urlpatterns = [
     path("", views.index, name="forum-index"),
+    path("forum/<pk:int>/", views.forum_detail, name="forum-detail"
 ]
--- app/forum/views.py
+++ app/forum/views.py
@@ -6,3 +6,8 @@ def index(request):
     forum_list = models.Forum.objects.all()
     return render(request, "forum/index.html", {"forum_list": forum_list})
 
+
+def forum_detail(request, pk):
+    forum_data = models.Forum.objects.get(pk=pk)
+    return render(request, "forum/forum-detail.html", {"forum_data": forum_data})
+
