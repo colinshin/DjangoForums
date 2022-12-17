65fead10df00a0b504d7b45bd60d5fbf27ee1c11
Josh Bright <josh.bright@gmail.com>
2022-12-17 00:06:10 -0800

    Added author field to forum post model

--- app/forum/models.py
+++ app/forum/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 
 
 class BaseModel(models.Model):
@@ -30,3 +31,4 @@ class ForumPost(BaseModel):
     title = models.CharField(max_length=256)
     views = models.IntegerField(default=0)
     content = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
