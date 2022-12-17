c0aac0bdd4163bce1c568e6cafcf8f8757615c5c
Josh Bright <josh.bright@gmail.com>
2022-12-16 21:32:16 -0800

    Added a forum category detail page

--- app/forum/urls.py
+++ app/forum/urls.py
@@ -4,4 +4,5 @@ from forum import views
 urlpatterns = [
     path("", views.index, name="forum-index"),
     path("forum/<int:pk>/", views.forum_detail, name="forum-detail"),
+    path("forum-category/<int:pk>/", views.forum_category_detail, name="forum-category-detail"),
 ]
--- app/forum/views.py
+++ app/forum/views.py
@@ -7,6 +7,11 @@ def index(request):
     return render(request, "forum/index.html", {"forum_cat_list": forum_cat_list})
 
 
+def forum_category_detail(request, pk):
+    forum_cat_data = models.ForumCategory.objects.get(pk=pk)
+    return render(request, "forum/forum-category-detail.html", {"cat_data": forum_cat_data})
+
+
 def forum_detail(request, pk):
     forum_data = models.Forum.objects.get(pk=pk)
     return render(request, "forum/forum-detail.html", {"forum_data": forum_data})
